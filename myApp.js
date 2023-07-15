ce38aa0f63837016674d00870ec351e351735ee0
Veronika Krukovich <veronikakrukovich@gmail.com>
2023-07-15 16:32:28 +0300

    add serve json myApp.js

--- myApp.js
+++ myApp.js
@@ -5,7 +5,7 @@ console.log("Hello World");
 app.get("/", function(req, res) {
     res.send("Hello Express");
   });
-*/
+
 
   app.get("/", function(req, res) {
     res.sendFile(__dirname + "/views/index.html");
@@ -15,9 +15,13 @@ app.get("/", function(req, res) {
   app.use(express.static(__dirname + "/public"));
 // Assets at the /public route
   app.use("/public", express.static(__dirname + "/public"));
+*/
 
-
-
+app.get("/json", function(req, res) {
+    res.json({
+        message: "Hello json"
+      });
+  });
 
 
 
